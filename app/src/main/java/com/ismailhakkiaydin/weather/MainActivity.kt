5ad8a06eb5a39cafffa81bfb1c7d3a907e77c2cd
ihaydinn <ihaydin7@gmail.com>
2020-04-13 10:39:20 +0300

    added bottom navigation controller

--- app/src/main/java/com/ismailhakkiaydin/weather/MainActivity.kt
+++ app/src/main/java/com/ismailhakkiaydin/weather/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.ismailhakkiaydin.weather
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var navController = findNavController(R.id.fragment)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
+
 }
